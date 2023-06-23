d30e58b0548908a6a7135958b67de7b4c51d1653
samuel <samuel.baker@rack-a-tiers.com>
2023-06-23 16:28:04 -0700

    - new commit

--- src/components/story.js
+++ src/components/story.js
@@ -4,11 +4,11 @@ import '../tailwind.css';
 
 const Story = ({ imageUrl, text, title }) => {
   return (
-    <div className='flex lg:flex-row justify-center sm:flex-col md:flex-col'>
-      <div className='lg:basis-1/4 my-auto order-1 sm:basis-full md:basis-full'>
+    <div className='flex lg:flex-row justify-center md:flex-col sm:flex-col'>
+      <div className='xl:basis-1/4 lg:basis-1/4 my-auto'>
         <img className='rounded-full drop-shadow-lg' src={imageUrl} alt="Sample" style={{ width: '200px' }} />
       </div>
-      <div className='lg:basis-1/2 order-2 sm:basis-full md:basis-full'>
+      <div className='xl:basis-1/2 lg:basis-1/2 order-2 sm:basis-full md:basis-full'>
         <h1 className='font-heading text-2xl font-bold'>{title}</h1>
         <p>{text}</p>
       </div>
