56c9f9854c82c1b80b2fa1cd7ac2b3f8a7a7d2ed
samuel <samuel@samuel.baker.ca>
2023-06-25 23:06:02 -0700

    - added more pictures

--- src/components/support.js
+++ src/components/support.js
@@ -33,6 +33,60 @@ const Support = ({ text, title }) => {
           </div>
         </div>
       </div>
+      <div className="flex items-center lg:flex-row mx-auto justify-center my-12 flex-col">
+        {/* product picture */}
+        <div className="md:basis-1/3 basis-full md:mx-12 mx-0">
+          <img
+            className="rounded-lg drop-shadow-lg"
+            src="https://www.meat-online.co.za/wp-content/uploads/2016/07/0444-BEEF-BILTONG-500g-e1471957916456.jpg"
+            alt="Product"
+          />
+        </div>
+        {/* selection && buy button */}
+        <div className="lg:basis-1/3">
+          <div className="flex items-center mt-4 space-x-4 justify-center text-black">
+            <div className="order-2">
+              <select className="ml-4">
+                <option value="1">1 Pack</option>
+                <option value="2">2 Pack</option>
+                <option value="3">3 Pack</option>
+              </select>
+            </div>
+            <div className="drop-shadow-lg order-3">
+              <button className="bg-darkPink text-white rounded px-4 py-2">
+                Buy Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center lg:flex-row mx-auto justify-center my-12 flex-col">
+        {/* product picture */}
+        <div className="md:basis-1/3 basis-full md:mx-12 mx-0">
+          <img
+            className="rounded-lg drop-shadow-lg"
+            src="https://www.meat-online.co.za/wp-content/uploads/2016/07/0444-BEEF-BILTONG-500g-e1471957916456.jpg"
+            alt="Product"
+          />
+        </div>
+        {/* selection && buy button */}
+        <div className="lg:basis-1/3">
+          <div className="flex items-center mt-4 space-x-4 justify-center text-black">
+            <div className="order-2">
+              <select className="ml-4">
+                <option value="1">1 Pack</option>
+                <option value="2">2 Pack</option>
+                <option value="3">3 Pack</option>
+              </select>
+            </div>
+            <div className="drop-shadow-lg order-3">
+              <button className="bg-darkPink text-white rounded px-4 py-2">
+                Buy Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
