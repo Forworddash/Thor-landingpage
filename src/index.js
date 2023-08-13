31739c12075f646bdb018817f0c92a9552138148
samuel <samuel.baker@rack-a-tiers.com>
2023-08-13 16:52:22 -0700

    - api support through shopify

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Client from 'shopify-buy';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = Client.buildClient({ storefrontAccessToken: '2e6d5348835d53c9b3c38069585360dd', domain: '45e729.myshopify.com' });
 root.render(
   <React.StrictMode>
     <App />
